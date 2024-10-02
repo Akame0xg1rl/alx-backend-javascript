be96449f64020d9d8d2fbdbb7a8cf6d55be9ad40
Your Name <ikrambouachra2@gmail.com>
2024-10-02 03:59:01 -0700

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    // Call signUpUser and uploadPhoto, and return a Promise that resolves when both are settled
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName)
-    ]).then(results => {
-        // Transform results to the desired structure
-        return results.map(result => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason
-        }));
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
